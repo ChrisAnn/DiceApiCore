8f572d8311e1bd3a4a763e70d59a21afacbc06b1
ChrisAnn <christine.ann.odell@gmail.com>
2017-09-30 16:52:32 +0100

    Adds DiceController and first test

---
+++ src/DiceApi.UnitTests/DiceControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using DiceApi.Controllers;
+using Xunit;
+
+namespace DiceApi.UnitTests
+{
+    public class DiceControllerTests
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(10)]
+        [InlineData(20)]
+        [InlineData(100)]
+        public void RollingADieGivesANumberBetween1AndTheDieSize(int size)
+        {
+            var controller = new DiceController();
+
+            var result = controller.Get(size);
+            
+            Assert.InRange(result, 1, size);
+        }
+    }
+}
---
+++ src/DiceApi/Controllers/DiceController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiceApi.Controllers
+{
+    public class DiceController : Controller
+    {
+        private readonly Random _random = new Random();
+        
+        public int Get(int i)
+        {
+            return _random.Next(1, i);
+        }
+    }
+}
